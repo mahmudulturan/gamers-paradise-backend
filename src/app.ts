0379d12903dc06c8772baf263d7c05a52c88686c
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-04-27 13:17:52 +0600

    intial commit: basic express server setup

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig();
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+
+// create app
+const app = express();
+
+
+// middlewares
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000" || "", process.env.LIVE_CLIENT_URL || ""],
+    credentials: true
+}));
+
+
+
+// home route of this server
+app.get('/', (req: Request, res: Response) => {
+    res.send("Wellcome to Barber Voyage server")
+})
+
+// response for not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send({ message: "The specified route cannot be located or identified." })
+})
+
+export default app;
