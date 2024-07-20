5524154820c797ca0ef9e6a43ae754568b9d40cb
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-07-21 01:38:02 +0600

    handle validation error on globalErrorHandler

--- src/errorHandlers/globalErrorHandler.ts
+++ src/errorHandlers/globalErrorHandler.ts
@@ -1,7 +1,43 @@
-import { ErrorRequestHandler, NextFunction } from "express"
+import { ErrorRequestHandler } from "express";
+import handleValidationError from "../errors/handleValidationError";
+import dot_env from "../configs/dotenv";
+
+export type TErrorSources = {
+    path: string | number,
+    message: string
+}[];
+
+
+export type TGenericErrorResponse = {
+    statusCode: number;
+    message: string;
+    errorSources: TErrorSources
+}
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(500).send({ message: err.message })
+
+    let status = 500;
+    let message = 'Something Went Wrong';
+    let errorMessages: TErrorSources = [
+        {
+            path: "",
+            message: ""
+        }
+    ]
+
+    if (err.name === 'ValidationError') {                 // handle mongoose validation error
+        const simplifiedMongooseError = handleValidationError(err);
+        errorMessages = simplifiedMongooseError.errorSources;
+        status = simplifiedMongooseError.statusCode;
+        message = simplifiedMongooseError.message;
+    } 
+
+    res.status(status).send({
+        success: false,
+        message,
+        errorMessages,
+        stack: dot_env.node_env === "development" ? err.stack : null
+    })
 }
 
 export default globalErrorHandler;
